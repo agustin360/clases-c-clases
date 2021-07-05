e6ade2612bf895e16c0878049443d194fb3dd0a8
Agustin <Agustin@MacBook-Air-de-Agustin.local>
2021-07-05 18:28:34 -0300

    primer commit

---
+++ index.ts
@@ -0,0 +1,26 @@
+class Album {
+    title: string
+    songs: string[]
+    constructor(title, songs) {
+        this.title = title
+        this.songs = songs
+    }
+}
+
+class Banda {
+    members: string[];
+    albums: Album[];
+    constructor(members: string[], albums: Album[]) {
+        this.members = members
+        this.albums = albums
+    }
+}
+
+const main = () => {
+    const unAlbum = new Album ("El album de Agus", ["lalalal"])
+    //console.log(unAlbum.title)
+    const unaBanda = new Banda (["Agustin", "Gonzalo"], [unAlbum])
+    console.log("Miembros: " + unaBanda.members); console.log("Albums: " + unAlbum.title)
+}
+
+main();
